e3695d299373bc6151cf403987ea023702054e4a
Dennis <dennis.basyrov@gmail.com>
2019-05-29 03:43:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 # require modules here
 
 def load_library
-  # code goes here
+  library = {"get_meaning" => {}, "get_emoticon" => {} }
+  YAML.load_file(file_path).each do |meaning, array|
+    english, japanese = array
+    library["get_emoticon"][english] = japanese
+    library["get_meaning"][japanese] = meaning
+  end
+  library
 end
 
 def get_japanese_emoticon
